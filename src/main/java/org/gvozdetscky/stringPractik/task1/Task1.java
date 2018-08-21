def4d5d3683f1923dee58a002f4285399bc28163
Егорка <gvozdetscky.egor@yandex.ru>
2018-08-21 19:04:32 +0300

    Добавил док для Task1

--- src/main/java/org/gvozdetscky/stringPractik/task1/Task1.java
+++ src/main/java/org/gvozdetscky/stringPractik/task1/Task1.java
@@ -1,7 +1,15 @@
 package org.gvozdetscky.stringPractik.task1;
 
+/**
+ * class for getting the length of str
+ */
 public class Task1
 {
+    /**
+     * method return length of str
+     * @param str str for getting the length
+     * @return length of str
+     */
     public int getLengthStr(String str) {
         return str.length();
     }
